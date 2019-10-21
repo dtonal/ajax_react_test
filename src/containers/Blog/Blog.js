26a921234002c5e88561bd2e3308d9b8cb6ba5ba
dtonal <dtonal@posteo.de>
2019-10-21 19:25:27 +0200

    Fetching data from JSONPlaceholder-API

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,19 +3,47 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 
+import Axios from 'axios';
+
 class Blog extends Component {
+
+    state = {posts: []};
+
+    componentDidMount(){
+        Axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Torben'
+                }
+            })
+            this.setState({posts: updatedPosts});
+        })
+    }
+
+    postSelectedHandler = (postId) => {
+        this.setState({selectedPostId: postId})
+    }
+
+
     render () {
+        const posts = this.state.posts.map(post => <Post 
+            title={post.title} 
+            author={post.author} 
+            key={post.id}
+            clicked={() => this.postSelectedHandler(post.id)}/>);
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
